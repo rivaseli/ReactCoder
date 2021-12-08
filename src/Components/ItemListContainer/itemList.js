743bec98938d8789b79f8129b5de8f80f4a84ab9
rivaseli <88515733+rivaseli@users.noreply.github.com>
2021-12-07 22:41:28 -0300

    desafio 5 incompleto

---
+++ src/Components/ItemListContainer/itemList.js
@@ -0,0 +1,25 @@
+import React , {useEffect, useState} from "react"
+import CircularProgress from '@mui/material/CircularProgress';
+
+const pastas = [
+    
+        {
+            "imagen" : "../src/Assets/tallarines.jpg.",
+            "nombre" : "Tallarines",
+            "precio" : "$180",
+            "id" : 1
+        },
+        {
+            "imagen" : "../src/Assets/cintas.jpg",
+            "nombre" : "Cintas",
+            "precio" : "$200",
+            "id" : 2
+        },
+        {
+            "imagen" : "../src/Assets/Ñoquis.jpg",
+            "nombre" : "Ñoquis",
+            "precio" : "$400",
+            "id" : 3
+        }
+    
+]
